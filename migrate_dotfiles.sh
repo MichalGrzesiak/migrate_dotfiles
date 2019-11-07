55136c1be3eda5d5b5ec36fb9af9698cecb25eaa
Michał Grzesiak <michal.grzesiak@sabre.com>
2019-11-07 03:19:20 +0100

    Added sourcing of bash aliases

--- migrate_dotfiles.sh
+++ migrate_dotfiles.sh
@@ -3,3 +3,6 @@ cd $HOME
 echo ".cfg" >> .gitignore
 git clone --bare https://github.com/MichalGrzesiak/dotfiles $HOME/.cfg
 /usr/bin/git --git-dir=$HOME/.cfg/ --work-tree=$HOME checkout
+
+echo "source  $HOME/.config/bash/bash_aliases.sh" >> $HOME/.bashrc
+
