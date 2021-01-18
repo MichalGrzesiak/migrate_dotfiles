60194c9bb3a814eb742a6bb43d67227db530f290
Michał Grzesiak <mgrzesiak147@gmail.com>
2021-01-18 15:54:03 +0100

    Added -f option to git checkout to forcefully overwrite any possible file conflicts.

--- migrate_dotfiles.sh
+++ migrate_dotfiles.sh
@@ -2,7 +2,7 @@ echo "alias config='/usr/bin/git --git-dir=$HOME/.cfg/ --work-tree=$HOME'" >> $H
 cd $HOME
 echo ".cfg" >> .gitignore
 git clone --bare https://github.com/MichalGrzesiak/dotfiles $HOME/.cfg
-/usr/bin/git --git-dir=$HOME/.cfg/ --work-tree=$HOME checkout
+/usr/bin/git --git-dir=$HOME/.cfg/ --work-tree=$HOME checkout -f
 
 echo "source  $HOME/.config/bash/bash_aliases.sh" >> $HOME/.bashrc
 echo 'export PATH="$HOME/.scripts:$PATH"' >> $HOME/.bashrc
